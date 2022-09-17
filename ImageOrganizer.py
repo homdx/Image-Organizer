363991f1bf7a5e1d07e2192e5133c0bbaa11ea82
Homdx <uqgvhom3dxto@bk.ru>
2022-09-17 14:52:28 +0300

    Fix error in opening image

--- ImageOrganizer.py
+++ ImageOrganizer.py
@@ -5,7 +5,7 @@ Created on Wed May  6 19:26:27 2020
 @author: Somil
 """
 
-#import PIL 
+#import PIL
 #from PIL.Image import core as _imaging
 from PIL import Image
 import os
@@ -21,61 +21,61 @@ class ImageOrganizer:
         self.images = [f for f in glob.glob(dirname + "**/*.jpg", recursive=True)]
         self.images = self.images + [f for f in glob.glob(dirname + "**/*.JPG", recursive=True)]
         self.dirname = dirname
-        
+
     def preprocess_exif(self,data):
         data = data.strip()
         data = data.strip('\x00')
-        
+
         return data
 
     def sort_by_device(self):
         for fname in self.images:
             with Image.open(os.path.join(self.dirname,fname)) as img:
-                exif = img._getexif() 
-            
+                exif = img._getexif()
+
             manuf = self.preprocess_exif(exif[271])
             device = self.preprocess_exif(exif[272])
             merged = manuf + ' ' + device
-            
+
             if not os.path.isdir(merged):
                 os.mkdir(merged)
-        
+
             shutil.move(os.path.join(self.dirname,fname),os.path.join(merged,fname))
             print("Image {} moved from {} to {} successfully\n".format(fname,os.path.join(self.dirname,fname),os.path.join(merged,fname)))
-            
-            
+
+
     def sort_by_year(self):
         for fname in self.images:
             with Image.open(os.path.join(self.dirname,fname)) as img:
-                exif = img._getexif() 
-            
+                exif = img._getexif()
+
             ts = self.preprocess_exif(exif[306])
             date = ts.split(' ')[0]
             year = datetime.datetime.strptime(date, '%Y:%m:%d').strftime('%Y')
-                     
+
             if not os.path.isdir(year):
                 os.mkdir(year)
-        
+
             shutil.copy(os.path.join(self.dirname,fname),os.path.join(year,fname))
             print("Image {} moved from {} to {} successfully\n".format(fname,os.path.join(self.dirname,fname),os.path.join(year,fname)))
-            
+
 
     def sort_by_yr_month(self):
         for fname in self.images:
             with Image.open(os.path.join(self.dirname,fname)) as img:
-                exif = img._getexif() 
-            
+                exif = img._getexif()
+
             ts = self.preprocess_exif(exif[306])
             date = ts.split(' ')[0]
             year = datetime.datetime.strptime(date, '%Y:%m:%d').strftime('%Y')
             month = datetime.datetime.strptime(date, '%Y:%m:%d').strftime('%b')
-                     
+
             if not os.path.isdir(year):
                 os.mkdir(year)
-            
+
             if not os.path.isdir(os.path.join(year,month)):
                 os.mkdir(os.path.join(year,month))
-        
+
             shutil.copy(os.path.join(self.dirname,fname),os.path.join(year,month,fname))
             print("Image {} moved from {} to {} successfully\n".format(fname,os.path.join(self.dirname,fname),os.path.join(year,month,fname)))
 
@@ -85,9 +85,10 @@ class ImageOrganizer:
             if os.path.isfile(fname) == True:
                 #with Image.open(os.path.join(self.dirname,fname)) as img:
                 current+=1
-                with Image.open(os.path.join(fname)) as img:
-                    exif = img._getexif()
                 try:
+                    with Image.open(os.path.join(fname)) as img:
+                        exif = img._getexif()
+#                try:
                     ts = self.preprocess_exif(exif[36867])
                     date = ts.split(' ')[0]
                     manuf = self.preprocess_exif(exif[271])
@@ -111,7 +112,7 @@ class ImageOrganizer:
                     #Update timestpamt for folder
                     folder = os.path.dirname(os.path.abspath(os.path.join(merged,year,month,os.path.basename(fname))))
                     os.utime(folder,(Unix_timestamp,Unix_timestamp))
-                    print("Image {} moved from {} to {} successfully\n".format(fname,os.path.join(fname),os.path.join(merged,year,month,os.path.basename(fname))), "  [",len(self.images),"/",current,"]")
+                    print("Image {} copied from {} to {} successfully\n".format(fname,os.path.join(fname),os.path.join(merged,year,month,os.path.basename(fname))), "  [", current," / ",len(self.images),"]")
                 except:
                     print("Error metadata from ",fname)
                     merged = "Undefined"
